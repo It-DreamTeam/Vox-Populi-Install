96603925c57bdefa491bc8f5d593c4de3f2c8703
nicolas <guigue.nico@gmail.com>
2017-08-28 10:37:36 +0200

    update env install script

--- Script/Hackaton_installl.sh
+++ Script/Hackaton_installl.sh
@@ -7,46 +7,41 @@
 #     Paramétrage CakePhp                           #
 #####################################################
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-else
-  test=0
-  while [ ${test} -eq 0 ]; do
-    read -s -p "Enter Password for root MySQL: `echo $'\n> '`" SqlPassword
-    echo `echo $'\n '`
-    read -s -p "Confirm Password for root MySQL: `echo $'\n> '`" SqlConfirmPass
-    echo `echo $'\n '`
-    if [ ${SqlPassword} = ${SqlConfirmPass} ]; then
-      test=1
-    fi
-  done
+# test=0
+# while [ ${test} -eq 0 ]; do
+#   read -s -p "Enter Password for root MySQL: `echo $'\n> '`" SqlPassword
+#   echo `echo $'\n '`
+#   read -s -p "Confirm Password for root MySQL: `echo $'\n> '`" SqlConfirmPass
+#   echo `echo $'\n '`
+#   if [ ${SqlPassword} = ${SqlConfirmPass} ]; then
+#     test=1
+#   fi
+# done
 
-  ### Création du Fichier Vagrantfile
-  touch Vagrantfile
-  echo "Vagrant.configure("2") do |config|" > Vagrantfile
-  echo "  config.vm.define \"web\" do |web|" >> Vagrantfile
-  echo "    web.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
-  echo "    web.vm.network \"private_network\", ip: \"192.168.33.11\"" >> Vagrantfile
-  echo "    web.vm.synced_folder \"./data\", \"/var/www/html\"" >> Vagrantfile
-  echo "    web.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'web'\"" >> Vagrantfile
-  # echo "    web.vm.provision \"shell\", path: \"../GitHubVersion/Vox-Populi-Install/Script/serevrInstall.sh\", :args => \"'web'\"" >> Vagrantfile
-  echo "  end" >> Vagrantfile
-  echo "  config.vm.define \"db\" do |db|" >> Vagrantfile
-  echo "    db.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
-  echo "    db.vm.network \"private_network\", ip: \"192.168.33.13\"" >> Vagrantfile
-  echo "    db.vm.synced_folder \"./data2\", \"/home/ubuntu\"" >> Vagrantfile
-  # echo "    db.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'db' '${SqlPassword}'\"" >> Vagrantfile
-  echo "    db.vm.provision \"shell\", path: \"./serverInstall.sh\", :args => \"'db'\"" >> Vagrantfile
-  echo "  end" >> Vagrantfile
-  # echo "  config.vm.define \"nagios\" do |nagios|" >> Vagrantfile
-  # echo "    db.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
-  # echo "    db.vm.network \"private_network\", ip: \"192.168.33.13\"" >> Vagrantfile
-  # echo "    db.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'nagios' '${SqlPassword}'\"" >> Vagrantfile
-  # echo "  end" >> Vagrantfile
-  echo "end" >> Vagrantfile
+### Création du Fichier Vagrantfile
+touch Vagrantfile
+echo "Vagrant.configure("2") do |config|" > Vagrantfile
+echo "  config.vm.define \"web\" do |web|" >> Vagrantfile
+echo "    web.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
+echo "    web.vm.network \"private_network\", ip: \"192.168.33.11\"" >> Vagrantfile
+echo "    web.vm.synced_folder \"./data\", \"/var/www/html\"" >> Vagrantfile
+# echo "    web.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'web'\"" >> Vagrantfile
+echo "    web.vm.provision \"shell\", path: \"./serverInstall.sh\", :args => \"'web'\"" >> Vagrantfile
+echo "  end" >> Vagrantfile
+echo "  config.vm.define \"db\" do |db|" >> Vagrantfile
+echo "    db.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
+echo "    db.vm.network \"private_network\", ip: \"192.168.33.12\"" >> Vagrantfile
+echo "    db.vm.synced_folder \"./data2\", \"/home/ubuntu\"" >> Vagrantfile
+# echo "    db.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'db' '${SqlPassword}'\"" >> Vagrantfile
+echo "    db.vm.provision \"shell\", path: \"./serverInstall.sh\", :args => \"'db'\"" >> Vagrantfile
+echo "  end" >> Vagrantfile
+# echo "  config.vm.define \"nagios\" do |nagios|" >> Vagrantfile
+# echo "    nagios.vm.box = \"ubuntu/xenial64\"" >> Vagrantfile
+# echo "    nagios.vm.network \"private_network\", ip: \"192.168.33.13\"" >> Vagrantfile
+# # echo "    nagios.vm.provision \"shell\", path: \"https://raw.githubusercontent.com/It-DreamTeam/Vox-Populi/master/Script/serverInstall.sh\", :args => \"'nagios' '${SqlPassword}'\"" >> Vagrantfile
+# echo "    nagios.vm.provision \"shell\", path: \"./serverInstall.sh\", :args => \"'nagios'\"" >> Vagrantfile
+# echo "  end" >> Vagrantfile
+echo "end" >> Vagrantfile
 
-  # Lancement de deux vagrant (Web and DB)
-  vagrant up
-
-fi
+# Lancement de deux vagrant (Web and DB)
+vagrant up
--- Script/serverInstall.sh
+++ Script/serverInstall.sh
@@ -11,6 +11,7 @@
 
 # Update repository
 apt-get update
+echo "ubuntu:azer" | sudo chpasswd
 
 # Install common for Web and DB server
 apt-get install -y nmap
@@ -19,35 +20,50 @@ if [ "${1}" = "web" ]; then
   cd /var/www/html
   # rootPasswd= ${1}
 
-  # Install Apache2 + Php7 + Php extension - libapache, mysql, intl, mbstring
-  apt-get install -y apache2 php libapache2-mod-php php-mysql php-intl php-mbstring
+  # Install Apache2 + Php7 + Php extension - libapache, mysql, intl, mbstring, xml
+  apt-get install -y apache2 php libapache2-mod-php php-mysql php-intl php-mbstring php-xml
 
-  # Install Unzip, Git,
-  apt-get install -y unzip git
-
-  # Install Composer
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-  php composer-setup.php
-  php -r "unlink('composer-setup.php');"
-  # mv composer.phar /usr/local/bin/composer
+  # Install Unzip, Git, composer
+  apt-get install -y unzip git composer
 
   # Install CakePhp
-  php composer.phar create-project --prefer-dist cakephp/app moodify
-  chmod +x ./moodify/bin/cake
-  php composer.phar require google/apiclient
-  php composer.phar update
+  composer create-project --prefer-dist cakephp/app moodify
+  cd moodify
+  chmod +x ./bin/cake
+
+  # Paramatrage Firewall
+  ufw allow 22
+  ufw allow 80
+  ufw allow 443
+  ufw --force enable
+
+  # Add Certificat SSL
+  openssl genrsa -out moodify.local.key 2048
+  openssl req -new -x509 -key moodify.local.key -out moodify.local.cert -days 3650 -subj /CN=moodify.local
+  mkdir /etc/apache2/sites-available/Ultimate
+  mv moodify.local.key /etc/apache2/sites-available/Ultimate/moodify.local.key
+  mv moodify.local.cert /etc/apache2/sites-available/Ultimate/moodify.local.cert
+
+
+  composer require google/apiclient
+  composer update
+
+  rm -r config src webroot README.md composer.json
+  git init
+  git remote add origin https://github.com/It-DreamTeam/Vox-Populi-Application.git
+  git pull origin master
 
   # Change User Apache user
   sed -i "s/www-data/ubuntu/g" /etc/apache2/envvars
-  # sed -i "s/www\/html/www\/html\/moodify\/webroot/g" /etc/apache2/sites-available/000-default.conf
+  # sed -i "s//www\/html\/moodify\/webroot/g" /etc/apache2/sites-available/000-default.conf
   a2enmod rewrite
-
 
   # Gestion Vhost
   echo "<VirtualHost *:80>" > /etc/apache2/sites-available/Ultimate.conf
-  echo "      ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      ServerAdmin alban@fakeMail.com" >> /etc/apache2/sites-available/Ultimate.conf
   echo "      DocumentRoot /var/www/html/moodify/webroot" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      ServerName moodify.local" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      Redirect permanent / https://moodify.local/" >> /etc/apache2/sites-available/Ultimate.conf
   echo "      ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/Ultimate.conf
   echo "      CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/Ultimate.conf
   echo "      <Directory />" >> /etc/apache2/sites-available/Ultimate.conf
@@ -61,11 +77,34 @@ if [ "${1}" = "web" ]; then
   echo "        Allow from all" >> /etc/apache2/sites-available/Ultimate.conf
   echo "      </Directory>" >> /etc/apache2/sites-available/Ultimate.conf
   echo "</VirtualHost>" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "<VirtualHost *:443>" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      ServerAdmin alban@fakeMail.com" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      DocumentRoot /var/www/html/moodify/webroot" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      <Directory />" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        Options FollowSymLinks" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        AllowOverride All" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      </Directory>" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      <Directory /var/www>" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        Options Indexes FollowSymLinks MultiViews" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        AllowOverride All" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        Order Allow,Deny" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "        Allow from all" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      </Directory>" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      # adding custom SSL cert" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      SSLEngine on" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      SSLCertificateFile /etc/apache2/sites-available/Ultimate/moodify.local.cert" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "      SSLCertificateKeyFile /etc/apache2/sites-available/Ultimate/moodify.local.key" >> /etc/apache2/sites-available/Ultimate.conf
+  echo "</VirtualHost>" >> /etc/apache2/sites-available/Ultimate.conf
   a2ensite Ultimate.conf
   a2dissite 000-default.conf
+  a2enmod ssl
 
   # retart Apache2
   service apache2 restart
+
+
 
   # Parameter webserver for cake PHP et récupération des fichiers
   # cd moodify
@@ -77,11 +116,32 @@ if [ "${1}" = "web" ]; then
   # git pull origin master
 
 elif [ "${1}" = "db" ];then
+
+  # Paramatrage Firewall
+  ufw allow 22
+  ufw allow 3306
+  ufw --force enable
+
   # Install Mysql Server
   echo "mysql-server mysql-server/root_password password 0000" | sudo debconf-set-selections
   echo "mysql-server mysql-server/root_password_again password 0000" | sudo debconf-set-selections
   bash -c "DEBIAN_FRONTEND='noninteractive' apt-get -y -q install mysql-server"
 
+  # Initialise Param of the DB and Authorize distant access
+  sed -i -e 's/\(bind-address\)/# \1/' /etc/mysql/mysql.conf.d/mysqld.cnf
+  echo "UPDATE mysql.user SET Host = '%' WHERE user.Host = 'localhost' AND user.User = 'root';" | mysql -uroot -p0000
+  sudo service mysql restart
+
+
   # create database cook
   mysql -uroot -p0000 < cook.sql
+
+# elif [ "${1}" = "nagios" ];then
+#   # Install Nagios 3
+#   export DEBIAN_FRONTEND="noninteractive"
+#
+#   sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword password 0000"
+#   sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword-repeat password 0000"
+#
+#   sudo apt-get install -y nagios3
 fi
